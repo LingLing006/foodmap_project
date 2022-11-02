57edcca8cb9cf65261d905acb50a86652ecb1f26
a0923 <a0923@LAPTOP-6CQO3NO3>
2022-11-02 16:15:05 +0800

    first commit

---
+++ src/main/java/com/example/foodmap_project/FoodmapProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodmap_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmapProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/foodmap_project/entity/FoodMap.java
@@ -0,0 +1,5 @@
+package com.example.foodmap_project.entity;
+
+public class FoodMap {
+
+}
